3fcabe758757fa7e05a6316b7f3797db9d4fa4c0
dzunn <nurroinz@gmail.com>
2022-04-07 18:30:10 +0700

    update duration

--- src/utils/showMessage/index.js
+++ src/utils/showMessage/index.js
@@ -5,6 +5,6 @@ export const showMessage = (message, type) => {
     message,
     type: type === 'success' ? 'success' : 'danger',
     backgroundColor: type === 'success' ? '#1ABC9C' : '#D9435E',
-    duration: 3000,
+    duration: 4000,
   });
 };
