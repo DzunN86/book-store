22a32008eb21cc820c2aa656f84fa9d6c722550b
dzunn <nurroinz@gmail.com>
2022-04-07 18:28:19 +0700

    buat page success register

--- src/screens/SuccessRegister/index.js
+++ src/screens/SuccessRegister/index.js
@@ -1,18 +1,25 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {Image, Text, View} from 'react-native';
+import {Button} from '../../components';
+import {images} from '../../themes';
+import styles from './styles';
 
-export default function RegisterSuccess() {
+export default function RegisterSuccess({navigation}) {
   return (
     <View style={styles.container}>
-      <Text>Register Success</Text>
+      {/* <StatusBar barStyle="dark-content" backgroundColor={COLORS.white} /> */}
+      <Image source={images.SuccesRegis} />
+      <Text style={styles.title}>Register Success</Text>
+      <Text style={styles.Subtitle}>
+        We sent email verification to your email
+      </Text>
+      <View style={styles.buttonWrapper}>
+        <Button
+          primary
+          title="Back to Login"
+          onPress={() => navigation.navigate('LoginScreen')}
+        />
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
