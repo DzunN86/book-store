ed88c01069b9668b5469dc06d8ce82e62bf83c69
dzunn <nurroinz@gmail.com>
2022-04-08 21:58:10 +0700

    create screen book detail

--- src/screens/BookDetail/index.js
+++ src/screens/BookDetail/index.js
@@ -1,18 +1,33 @@
-import {StyleSheet, Text, View} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import {BookDesc, BookInfo, BottomButton} from '../../components';
+import {COLORS} from '../../themes';
 
-export default function BookDetail() {
+export default function BookDetail({navigation, route}) {
+  const {id} = route.params;
+
   return (
     <View style={styles.container}>
-      <Text>BookDetail</Text>
+      {/* Book Cover Section */}
+      <View style={{flex: 4}}>
+        <BookInfo navigation={() => navigation.goBack()} />
+      </View>
+
+      {/* Description */}
+      <View style={{flex: 2}}>
+        <BookDesc />
+      </View>
+
+      {/* Buttons */}
+      <View style={styles.btnBottom}>
+        <BottomButton />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+  container: {flex: 1, backgroundColor: COLORS.black},
+  btnBottom: {height: 70, marginBottom: 25},
 });
