be0a0a97a7895f6e7fcbd024fbf684525f228b9e
dzunn <nurroinz@gmail.com>
2022-04-08 23:01:41 +0700

    add loading state

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import {
   ActivityIndicator,
   FlatList,
+  Image,
   RefreshControl,
   SafeAreaView,
   ScrollView,
@@ -11,9 +12,10 @@ import {
   View,
 } from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
-import {HeaderUser, RecomCard} from '../../components';
-import {fetchBook, refreshBook} from '../../store/actions';
-import {COLORS, FONTS, SIZES} from '../../themes';
+import {HeaderUser, PopularCard, RecomCard} from '../../components';
+import {fetchBook} from '../../store/actions';
+import {REFRESH_BOOK} from '../../store/types';
+import {COLORS, FONTS, images, SIZES} from '../../themes';
 
 export default function Home({navigation}) {
   const dispatch = useDispatch();
@@ -27,56 +29,87 @@ export default function Home({navigation}) {
   }, [dispatch]);
 
   const onRefresh = () => {
-    dispatch(refreshBook());
+    dispatch({type: REFRESH_BOOK});
     dispatch(fetchBook());
   };
+
+  function renderWentWrong() {
+    return (
+      <View style={styles.WentWrong}>
+        <Image source={images.WentWrong} />
+        <Text style={styles.titleWrong}>Something went wrong</Text>
+        <Text style={styles.SubtitleWrong}>
+          Try to reload the page or check your internet connection
+        </Text>
+      </View>
+    );
+  }
 
   function renderRecomendedBook() {
     return (
       <>
-        {isLoading ? (
-          <ActivityIndicator size="large" color={COLORS.primary} />
-        ) : (
-          <View style={styles.bookSec}>
-            <View style={styles.bookHeader}>
-              <Text style={styles.title}>My Book</Text>
+        <View style={styles.bookSec}>
+          <View style={styles.bookHeader}>
+            <Text style={styles.title}>Recommended</Text>
 
-              <TouchableOpacity onPress={() => console.log('See More')}>
-                <Text style={styles.seeMore}>see more</Text>
-              </TouchableOpacity>
-            </View>
-            {/* Books */}
-            <View style={{flex: 1, marginTop: SIZES.padding}}>
-              <FlatList
-                data={books}
-                renderItem={({item, index}) => {
-                  return (
-                    <RecomCard
-                      item={item}
-                      index={index}
-                      navigation={() =>
-                        navigation.navigate('BookDetailScreen', {id: item.id})
-                      }
-                    />
-                  );
-                }}
-                keyExtractor={item => `${item.id}`}
-                horizontal
-                showsHorizontalScrollIndicator={false}
-              />
-            </View>
+            <TouchableOpacity onPress={() => console.log('See More')}>
+              <Text style={styles.seeMore}>see more</Text>
+            </TouchableOpacity>
           </View>
-        )}
+          {/* Books */}
+          <View style={{flex: 1, marginTop: SIZES.padding}}>
+            <FlatList
+              data={books}
+              renderItem={({item, index}) => {
+                return (
+                  <RecomCard
+                    item={item}
+                    index={index}
+                    navigation={() =>
+                      navigation.navigate('BookDetailScreen', {id: item.id})
+                    }
+                  />
+                );
+              }}
+              keyExtractor={item => `${item.id}`}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+            />
+          </View>
+        </View>
       </>
     );
   }
 
-  function renderPopulerHeader() {
-    return <></>;
-  }
-
   function renderPopulerBook() {
-    return <></>;
+    return (
+      <>
+        <View style={styles.bookSec}>
+          <View style={styles.bookHeader}>
+            <Text style={styles.title}>Popular Book</Text>
+
+            <TouchableOpacity onPress={() => console.log('See More')}>
+              <Text style={styles.seeMore}>see more</Text>
+            </TouchableOpacity>
+          </View>
+          {/* Books */}
+          <View style={styles.listPopuler}>
+            {books.map((item, index) => {
+              return (
+                <PopularCard
+                  item={item}
+                  key={index}
+                  index={index}
+                  navigation={() =>
+                    navigation.navigate('BookDetailScreen', {id: item.id})
+                  }
+                />
+              );
+            })}
+          </View>
+        </View>
+      </>
+    );
   }
 
   return (
@@ -95,15 +128,23 @@ export default function Home({navigation}) {
             onRefresh={() => onRefresh()}
           />
         }>
-        {/* Recomended Book */}
-        <View>{renderRecomendedBook()}</View>
+        {books ? (
+          <>
+            {isLoading ? (
+              <ActivityIndicator size="large" color={COLORS.primary} />
+            ) : (
+              <>
+                <View>{renderRecomendedBook()}</View>
 
-        {/* Popouler Book */}
-        <View style={styles.populerSec}>
-          <View>{renderRecomendedBook()}</View>
-          {/* <View>{renderPopulerHeader()}</View>
-          <View>{renderPopulerBook()}</View> */}
-        </View>
+                <View style={styles.populerSec}>
+                  <View>{renderPopulerBook()}</View>
+                </View>
+              </>
+            )}
+          </>
+        ) : (
+          renderWentWrong()
+        )}
       </ScrollView>
     </SafeAreaView>
   );
@@ -127,9 +168,32 @@ const styles = StyleSheet.create({
   title: {...FONTS.h2, color: COLORS.white},
   seeMore: {
     ...FONTS.body3,
-    color: COLORS.lightGray,
+    color: COLORS.lightGray3,
     alignSelf: 'flex-start',
     textDecorationLine: 'underline',
   },
   populerSec: {marginTop: SIZES.padding},
+  WentWrong: {
+    height: SIZES.height / 2,
+    marginTop: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  titleWrong: {
+    ...FONTS.h2,
+    color: COLORS.white,
+    marginTop: 20,
+    textAlign: 'center',
+  },
+  SubtitleWrong: {
+    ...FONTS.body3,
+    color: COLORS.lightGray3,
+    marginTop: 20,
+    textAlign: 'center',
+  },
+  listPopuler: {
+    flex: 1,
+    marginTop: SIZES.padding,
+    paddingLeft: SIZES.padding,
+  },
 });
