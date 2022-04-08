1888f7669a013d0274bea5902b2870093a3274d6
dzunn <nurroinz@gmail.com>
2022-04-08 21:58:27 +0700

    update components

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,4 +1,5 @@
 import Input from './Input';
 import Button from './Button';
+import LineDivider from './LineDivider';
 
-export {Input, Button};
+export {Input, Button, LineDivider};
--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -2,5 +2,6 @@ import Header from './Header';
 import HeaderUser from './HeaderUser';
 import ButtonFeature from './ButtonFeature';
 import RecomCard from './RecomCard';
+import PopularCard from './PopularCard';
 
-export {Header, HeaderUser, ButtonFeature, RecomCard};
+export {Header, HeaderUser, ButtonFeature, RecomCard, PopularCard};
