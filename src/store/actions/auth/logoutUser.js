60fed4bc17b59eaf5df7af713fc9827832b7e084
dzunn <nurroinz@gmail.com>
2022-04-08 22:59:29 +0700

    ketika logout => hapus state book

--- src/store/actions/auth/logoutUser.js
+++ src/store/actions/auth/logoutUser.js
@@ -1,7 +1,10 @@
-import {LOGOUT_USER} from '../../types';
+import {LOGOUT_USER, REMOVE_BOOK} from '../../types';
 
 export const logout = () => dispatch => {
   dispatch({
     type: LOGOUT_USER,
+  });
+  dispatch({
+    type: REMOVE_BOOK,
   });
 };
