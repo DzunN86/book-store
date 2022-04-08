d4f81248439650797c194a18233d62c8ab14d0c9
dzunn <nurroinz@gmail.com>
2022-04-08 23:00:42 +0700

    update components

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -3,5 +3,6 @@ import HeaderUser from './HeaderUser';
 import ButtonFeature from './ButtonFeature';
 import RecomCard from './RecomCard';
 import PopularCard from './PopularCard';
+import Loading from './Loading';
 
-export {Header, HeaderUser, ButtonFeature, RecomCard, PopularCard};
+export {Header, HeaderUser, ButtonFeature, RecomCard, PopularCard, Loading};
