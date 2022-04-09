b92b144d95426fa42d284e74e9323b231835400f
dzunn <nurroinz@gmail.com>
2022-04-09 14:08:33 +0700

    update compoenents

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -4,5 +4,16 @@ import ButtonFeature from './ButtonFeature';
 import RecomCard from './RecomCard';
 import PopularCard from './PopularCard';
 import Loading from './Loading';
+import OfflineInfo from './OfflineInfo';
+import ErrorConnection from './ErrorConnection';
 
-export {Header, HeaderUser, ButtonFeature, RecomCard, PopularCard, Loading};
+export {
+  Header,
+  HeaderUser,
+  ButtonFeature,
+  RecomCard,
+  PopularCard,
+  Loading,
+  OfflineInfo,
+  ErrorConnection,
+};
