308e8f4651dcf3feb16a893154c89503a436792f
dzunn <nurroinz@gmail.com>
2022-04-09 15:56:04 +0700

    delete unuse images

--- src/themes/images.js
+++ src/themes/images.js
@@ -1,16 +1,8 @@
-export const otherWordsForHome = require('../assets/images/other_words_for_home.jpg');
-export const theMetropolist = require('../assets/images/the_metropolist.jpg');
-export const theTinyDragon = require('../assets/images/the_tiny_dragon.jpg');
-export const underland = require('../assets/images/underland.jpg');
 export const Logo = require('../assets/images/logo.png');
 export const SuccesRegis = require('../assets/images/success-register.png');
 export const WentWrong = require('../assets/images/goes-error.png');
 
 export default {
-  otherWordsForHome,
-  theMetropolist,
-  theTinyDragon,
-  underland,
   Logo,
   SuccesRegis,
   WentWrong,
