c2ad59a93c3578e62ca8e5ce4074e3d6e26c15c9
dzunn <nurroinz@gmail.com>
2022-04-09 22:07:05 +0700

    add regex for email and passwrod validation

---
+++ src/utils/Regex/index.js
@@ -0,0 +1,4 @@
+export const EMAIL_REGEX =
+  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+export const PASSWORD_REGEX = /^(?=.{8,}$)(?=.*[a-z])(?=.*[0-9]).*$/;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
 export * from './showMessage';
 export * from './storage';
 export * from './notifikasi';
+export * from './Regex';
