3fde14bc3e76fdf6c2e00f83cd262d74ff971820
dzunn <nurroinz@gmail.com>
2022-04-09 14:11:14 +0700

    net info setting up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
+import NetInfo from '@react-native-community/netinfo';
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import FlashMessage from 'react-native-flash-message';
-import {Provider} from 'react-redux';
+import {Provider, useDispatch} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
+import {OfflineInfo} from './components';
 import Navigation from './navigation';
 import {Persistore, Store} from './store';
+import {setOnline} from './store/actions';
 
 const MainApp = () => {
+  const dispatch = useDispatch();
+
+  const removeNetInfo = () => {
+    NetInfo.addEventListener(state => {
+      const offline = !(state.isConnected && state.isInternetReachable);
+      dispatch(setOnline(!offline));
+    });
+  };
+
+  useEffect(() => {
+    removeNetInfo();
+    return () => {
+      removeNetInfo();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <NavigationContainer>
       <Navigation />
-      <FlashMessage position="bottom" />
+      <FlashMessage position="top" />
+      <OfflineInfo />
     </NavigationContainer>
   );
 };
